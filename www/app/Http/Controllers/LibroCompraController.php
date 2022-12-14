c17ce621a52ce3bd7211e69fb2fc95235008e23f
jona872 <jona_872@hotmail.com>
2022-12-13 22:40:37 -0300

    pre compra notebook

--- www/app/Http/Controllers/LibroCompraController.php
+++ www/app/Http/Controllers/LibroCompraController.php
@@ -38,7 +38,7 @@ class LibroCompraController extends Controller
                 ->join('clientes as c', 'lc.sender_id', '=', 'c.id')
                 ->select('c.*', 'lc.*')
                 ->get();
-            dd($relevamientos);
+            // dd($relevamientos);
 
             return view('compras.index', compact('compras'));
         } catch (Exception $e) {
@@ -50,34 +50,34 @@ class LibroCompraController extends Controller
         }
 
 
-        +"id": 2
-        +"name": "Angelica"
-        +"cuit": "cuit de la bd"
-        +"condition": "Responsable Inscripto"
-        +"direction": "Av. Almafuertes 4777"
-        +"activity_start": "12/2/2022"
-        +"gross_receipts_tax": "12123123"
-        +"created_at": null
-        +"updated_at": null
-        +"sender_id": 2
-        +"receiver_id": 1
-        +"fecha": "2022-12-13"
-        +"pto_venta": "001"
-        +"codigo": "00001"
-        +"tipo_comprobante": "Otros"
-        +"nombre": "nombre de la bd"
-        +"condicion": "condicion de la bd"
-        +"neto": null
-        +"iva": null
-        +"iva_liquidado": null
-        +"iva_sobretasa": null
-        +"percepcion": null
-        +"iva_retencion": null
-        +"impuestos_internos": null
-        +"conceptos_no_gravados": null
-        +"compras_no_inscriptas": null
-        +"total": null
-        +"tipo_op": null
+        // +"id": 2
+        // +"name": "Angelica"
+        // +"cuit": "cuit de la bd"
+        // +"condition": "Responsable Inscripto"
+        // +"direction": "Av. Almafuertes 4777"
+        // +"activity_start": "12/2/2022"
+        // +"gross_receipts_tax": "12123123"
+        // +"created_at": null
+        // +"updated_at": null
+        // +"sender_id": 2
+        // +"receiver_id": 1
+        // +"fecha": "2022-12-13"
+        // +"pto_venta": "001"
+        // +"codigo": "00001"
+        // +"tipo_comprobante": "Otros"
+        // +"nombre": "nombre de la bd"
+        // +"condicion": "condicion de la bd"
+        // +"neto": null
+        // +"iva": null
+        // +"iva_liquidado": null
+        // +"iva_sobretasa": null
+        // +"percepcion": null
+        // +"iva_retencion": null
+        // +"impuestos_internos": null
+        // +"conceptos_no_gravados": null
+        // +"compras_no_inscriptas": null
+        // +"total": null
+        // +"tipo_op": null
 
 
 
