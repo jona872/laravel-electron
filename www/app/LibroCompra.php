607ddc0983a5a23309d416fd6e6969be643cf160
jona872 <jona_872@hotmail.com>
2023-01-03 16:28:44 -0300

    models tipo_calculo

--- www/app/LibroCompra.php
+++ www/app/LibroCompra.php
@@ -13,7 +13,7 @@ class LibroCompra extends Model
         'fecha', 'pto_venta', 'codigo_comprobante', 'tipo_comprobante', 
         'receiver_id', 'sender_id',
         'neto', 'iva','iva_liquidado', 'iva_sobretasa', 'percepcion','iva_retencion', 
-        'impuestos_internos', 'conceptos_no_gravados', 'compras_no_inscriptas', 'total', 'tipo_op'
+        'impuestos_internos', 'conceptos_no_gravados', 'compras_no_inscriptas', 'total', 'tipo_op', 'tipo_calculo'
     ];
     
     protected $hidden = [];
