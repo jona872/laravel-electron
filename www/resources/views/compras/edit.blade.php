486d5138e06ae55a6c22c00cba5c51ff5613f56e
jona <jona@localhost.localdomain>
2023-01-16 18:32:26 -0300

    compras edit

--- www/resources/views/compras/edit.blade.php
+++ www/resources/views/compras/edit.blade.php
@@ -1,6 +1,7 @@
 @extends('layout')
 @push('styles')
 <link rel="stylesheet" href="{{ asset('css/compras.css')}}">
+<link rel="stylesheet" href="{{ asset('css/form.css') }}">
 @endpush
 
 @section('content')
@@ -18,103 +19,104 @@
     @csrf
     @method('PUT')
     <input name="id" type="hidden" value="{{ $compra->id }}">
-    <div>
-        <label for="fecha"> Fecha </label>
-        <input id="fecha" type="text" name="fecha" value="{{ $compra->fecha }}" />
-    </div>
+    <div class="container">
 
-    <div>
-        <label for="pto_venta"> Pto de Venta </label>
-        <input id="pto_venta" type="text" name="pto_venta" value="{{ $compra->pto_venta }}" />
-    </div>
+        <div class="left--panel">
+            <div class="form--row">
+                <label for="fecha"> Fecha </label>
+                <input id="fecha" type="text" name="fecha" value="{{ $compra->fecha }}" />
+            </div>
 
-    <div>
-        <label for="codigo_comprobante"> Nro Comprobante </label>
-        <input id="codigo_comprobante" type="text" name="codigo_comprobante" value="{{ $compra->codigo_comprobante }}" />
-    </div>
+            <div class="form--row">
+                <label for="pto_venta"> Pto de Venta </label>
+                <input id="pto_venta" type="text" name="pto_venta" value="{{ $compra->pto_venta }}" />
+            </div>
 
-    <div>
-        <label for="tipo_comprobante"> Tipo de Comprobante </label>
-        <select name="tipo_comprobante" id="tipo_comprobante">
-            <option selected="selected"> {{ $compra->tipo_comprobante }} </option>
-            <option value="Otros">Otros</option>
-            <option value="Factura A">Factura A</option>
-            <option value="Factura B">Factura B</option>
-            <option value="Factura C">Factura C</option>
-        </select>
-    </div>
-    <br>
-    <hr>
-    <br>
-    <div>
-        <label for="client_id"> Codigo Cliente </label>
-        <input readonly id="client_id" type="text" name="client_id" value="{{ $cliente->id }}" />
-    </div>
-    <div>
-        <label for="name"> Nombre Cliente </label>
-        <input readonly id="name" type="text" name="name" value="{{ $cliente->name }}" />
-    </div>
-    <div>
-        <label for="cuit"> CUIT </label>
-        <input readonly id="cuit" type="text" name="cuit" value="{{ $cliente->cuit }}" />
-    </div>
-    <div>
-        <label for="condition"> Condicion </label>
-        <input readonly id="condition" type="text" name="condition" value="{{ $cliente->condition }}" />
-    </div>
-    <br>
-    <hr>
-    <br>
-    <div>
-        <label for="neto"> Neto </label>
-        <input id="neto" type="text" name="neto" value="{{ $compra->neto }}" />
-    </div>
-    <div>
-        <label for="iva"> I.V.A. </label>
-        <input id="iva" type="text" name="iva" value="{{ $compra->iva }}" />
-    </div>
-    <div>
-        <label for="iva_liquidado"> I.V.A. Liquidado </label>
-        <input id="iva_liquidado" type="text" name="iva_liquidado" value="{{ $compra->iva_liquidado }}" />
-    </div>
-    <div>
-        <label for="iva_sobretasa"> Sobre Ta. I.V.A. </label>
-        <input id="iva_sobretasa" type="text" name="iva_sobretasa" value="{{ $compra->iva_sobretasa }}" />
-    </div>
-    <div>
-        <label for="percepcion"> Percepcion </label>
-        <input id="percepcion" type="text" name="percepcion" value="{{ $compra->percepcion }}" />
-    </div>
-    <div>
-        <label for="iva_retencion"> I.V.A. Retencion </label>
-        <input id="iva_retencion" type="text" name="iva_retencion" value="{{ $compra->iva_retencion }}" />
-    </div>
-    <div>
-        <label for="impuestos_internos"> impuestos_internos </label>
-        <input id="impuestos_internos" type="text" name="impuestos_internos" value="{{ $compra->impuestos_internos }}" />
-    </div>
-    <div>
-        <label for="conceptos_no_gravados"> conceptos_no_gravados </label>
-        <input id="conceptos_no_gravados" type="text" name="conceptos_no_gravados" value="{{ $compra->conceptos_no_gravados }}" />
-    </div>
-    <div>
-        <label for="compras_no_inscriptas"> compras_no_inscriptas </label>
-        <input id="compras_no_inscriptas" type="text" name="compras_no_inscriptas" value="{{ $compra->compras_no_inscriptas }}" />
-    </div>
-    <div>
-        <label for="tipo_calculo"> Tipo de Calculo </label>
-        <input id="tipo_calculo" type="text" step="any" name="tipo_calculo" value="{{ $compra->tipo_calculo }}" />
-    </div>
-    <br>
-    <hr>
-    <br>
-    <div>
-        <label for="total"> Total </label>
-        <input id="total" type="text" step="any" name="total" value="{{ $compra->total }}" />
-    </div>
-    <div>
-        <label for="tipo_op"> Tipo de Operacion </label>
-        <input id="tipo_op" type="text" step="any" name="tipo_op" value="{{ $compra->tipo_op }}" />
+            <div class="form--row">
+                <label for="codigo_comprobante"> Nro Comprobante </label>
+                <input id="codigo_comprobante" type="text" name="codigo_comprobante" value="{{ $compra->codigo_comprobante }}" />
+            </div>
+
+            <div class="form--row">
+                <label for="tipo_comprobante"> Tipo de Comprobante </label>
+                <select name="tipo_comprobante" id="tipo_comprobante">
+                    <option selected="selected"> {{ $compra->tipo_comprobante }} </option>
+                    <option value="Otros">Otros</option>
+                    <option value="Factura A">Factura A</option>
+                    <option value="Factura B">Factura B</option>
+                    <option value="Factura C">Factura C</option>
+                </select>
+            </div>
+
+            <div class="form--row">
+                <label for="client_id"> Codigo Cliente </label>
+                <input readonly id="client_id" type="text" name="client_id" value="{{ $cliente->id }}" />
+            </div>
+            <div class="form--row">
+                <label for="name"> Nombre Cliente </label>
+                <input readonly id="name" type="text" name="name" value="{{ $cliente->name }}" />
+            </div>
+            <div class="form--row">
+                <label for="cuit"> CUIT </label>
+                <input readonly id="cuit" type="text" name="cuit" value="{{ $cliente->cuit }}" />
+            </div>
+            <div class="form--row">
+                <label for="condition"> Condicion </label>
+                <input readonly id="condition" type="text" name="condition" value="{{ $cliente->condition }}" />
+            </div>
+        </div>
+
+        <div class="right--panel">
+            <div class="form--row">
+                <label for="neto"> Neto </label>
+                <input id="neto" type="text" name="neto" value="{{ $compra->neto }}" />
+            </div>
+            <div class="form--row">
+                <label for="iva"> I.V.A. </label>
+                <input id="iva" type="text" name="iva" value="{{ $compra->iva }}" />
+            </div>
+            <div class="form--row">
+                <label for="iva_liquidado"> I.V.A. Liquidado </label>
+                <input id="iva_liquidado" type="text" name="iva_liquidado" value="{{ $compra->iva_liquidado }}" />
+            </div>
+            <div class="form--row">
+                <label for="iva_sobretasa"> Sobre Ta. I.V.A. </label>
+                <input id="iva_sobretasa" type="text" name="iva_sobretasa" value="{{ $compra->iva_sobretasa }}" />
+            </div>
+            <div class="form--row">
+                <label for="percepcion"> Percepcion </label>
+                <input id="percepcion" type="text" name="percepcion" value="{{ $compra->percepcion }}" />
+            </div>
+            <div class="form--row">
+                <label for="iva_retencion"> I.V.A. Retencion </label>
+                <input id="iva_retencion" type="text" name="iva_retencion" value="{{ $compra->iva_retencion }}" />
+            </div>
+            <div class="form--row">
+                <label for="impuestos_internos"> impuestos_internos </label>
+                <input id="impuestos_internos" type="text" name="impuestos_internos" value="{{ $compra->impuestos_internos }}" />
+            </div>
+            <div class="form--row">
+                <label for="conceptos_no_gravados"> conceptos_no_gravados </label>
+                <input id="conceptos_no_gravados" type="text" name="conceptos_no_gravados" value="{{ $compra->conceptos_no_gravados }}" />
+            </div>
+            <div class="form--row">
+                <label for="compras_no_inscriptas"> compras_no_inscriptas </label>
+                <input id="compras_no_inscriptas" type="text" name="compras_no_inscriptas" value="{{ $compra->compras_no_inscriptas }}" />
+            </div>
+            <div class="form--row">
+                <label for="tipo_calculo"> Tipo de Calculo </label>
+                <input id="tipo_calculo" type="text" step="any" name="tipo_calculo" value="{{ $compra->tipo_calculo }}" />
+            </div>
+
+            <div class="form--row">
+                <label for="total"> Total </label>
+                <input id="total" type="text" step="any" name="total" value="{{ $compra->total }}" />
+            </div>
+            <div class="form--row">
+                <label for="tipo_op"> Tipo de Operacion </label>
+                <input id="tipo_op" type="text" step="any" name="tipo_op" value="{{ $compra->tipo_op }}" />
+            </div>
+        </div>
     </div>
 
     <div class="card--row">
