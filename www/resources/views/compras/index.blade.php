a50d7cdd995d95c9a8e2a92cf33b6539fc339c03
jona <jona@localhost.localdomain>
2023-01-16 19:05:39 -0300

    compra/venta fix create width

--- www/resources/views/compras/index.blade.php
+++ www/resources/views/compras/index.blade.php
@@ -14,7 +14,7 @@
     <div class="container">
 
 
-        <a href="compras/create" class="header-btn header-left">
+        <a href="compras/create" class="header-btn header-left fit-cnt">
             <div class="btn-icon">
                 <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
