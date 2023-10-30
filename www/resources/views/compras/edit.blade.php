54ac806904fab735291a609ea35d1b4ffdc17da0
jona872 <jona_872@hotmail.com>
2023-10-30 18:11:51 -0300

    compras edit view

--- www/resources/views/compras/edit.blade.php
+++ www/resources/views/compras/edit.blade.php
@@ -50,19 +50,19 @@
 
             <div class="form--row">
                 <label for="client_id"> Codigo Cliente </label>
-                <input  id="client_id" type="text" name="client_id" value="{{ $cliente->id }}" />
+                <input id="client_id" type="text" name="client_id" value="{{ $cliente->id }}" />
             </div>
             <div class="form--row">
                 <label for="name"> Nombre Cliente </label>
-                <input  id="name" type="text" name="name" value="{{ $cliente->name }}" />
+                <input id="name" type="text" name="name" value="{{ $cliente->name }}" />
             </div>
             <div class="form--row">
                 <label for="cuit"> CUIT </label>
-                <input  id="cuit" type="text" name="cuit" value="{{ $cliente->cuit }}" />
+                <input id="cuit" type="text" name="cuit" value="{{ $cliente->cuit }}" />
             </div>
             <div class="form--row">
                 <label for="condition"> Condicion </label>
-                <input  id="condition" type="text" name="condition" value="{{ $cliente->condition }}" />
+                <input id="condition" type="text" name="condition" value="{{ $cliente->condition }}" />
             </div>
         </div>
 
@@ -120,128 +120,152 @@
     </div>
 
     <div class="row text-center mt-3">
-    <div class="d-flex justify-content-center gap-2 flex-wrap">
-        <a class="btn btn-danger" href="{{ route('compras.index') }}">{{ __('Cancelar') }}</a>
+        <div class="d-flex justify-content-center gap-2 flex-wrap">
+            <a class="btn btn-danger" href="{{ route('compras.index') }}">{{ __('Cancelar') }}</a>
 
-        <button type="submit" class="btn btn-primary"> Guardar </button>
+            <button type="submit" class="btn btn-primary"> Guardar </button>
         </div>
-  </div>
+    </div>
 
 </form>
 <script src="{{ asset('js/jquery-3.6.1.min.js') }}"></script>
 <script src="{{ asset('js/jquery-ui.min.js') }}"></script>
 
 <script type="text/javascript">
-  let dataGlobal;
-  let globalClientClienteCode = [];
-  let globalClientClienteName = [];
-  let globalClientClienteCuit = [];
+    let dataGlobal;
+    let globalClientClienteCode = [];
+    let globalClientClienteName = [];
+    let globalClientClienteCuit = [];
 
 
-  let a1_neto = document.getElementById('neto');
-  let a2_iva = document.getElementById('iva');
-  let a3_iva_liquidado = document.getElementById('iva_liquidado');
-  let a4_iva_sobretasa = document.getElementById('iva_sobretasa');
-  let a5_percepcion = document.getElementById('percepcion');
-  let a6_iva_retencion = document.getElementById('iva_retencion');
-  let a7_impuestos_internos = document.getElementById('impuestos_internos');
-  let a8_conceptos_no_gravados = document.getElementById('conceptos_no_gravados');
-  let a9_compras_no_inscriptas = document.getElementById('compras_no_inscriptas');
-  let a11_total = document.getElementById('total');
-  // let editableButtons = [ a1_neto, a3_iva_liquidado, a4_iva_sobretasa, a5_percepcion, a6_iva_retencion, a7_impuestos_internos, a8_conceptos_no_gravados, a9_compras_no_inscriptas ];
+    let a1_neto = document.getElementById('neto');
+    let a2_iva = document.getElementById('iva');
+    let a3_iva_liquidado = document.getElementById('iva_liquidado');
+    let a4_iva_sobretasa = document.getElementById('iva_sobretasa');
+    let a5_percepcion = document.getElementById('percepcion');
+    let a6_iva_retencion = document.getElementById('iva_retencion');
+    let a7_impuestos_internos = document.getElementById('impuestos_internos');
+    let a8_conceptos_no_gravados = document.getElementById('conceptos_no_gravados');
+    let a9_compras_no_inscriptas = document.getElementById('compras_no_inscriptas');
+    let a11_total = document.getElementById('total');
+    // let editableButtons = [ a1_neto, a3_iva_liquidado, a4_iva_sobretasa, a5_percepcion, a6_iva_retencion, a7_impuestos_internos, a8_conceptos_no_gravados, a9_compras_no_inscriptas ];
 
-  function clearFields(vButtons) {
-    vButtons.map((btn) => {
-      btn.value = parseFloat('0').toFixed(2);
+    function clearFields(vButtons) {
+        vButtons.map((btn) => {
+            btn.value = parseFloat('0').toFixed(2);
+        });
+    }
+
+    function calcular() {
+        switch (document.getElementById("tipo_calculo").value) {
+            case '1':
+                clearFields([a1_neto, a3_iva_liquidado, a4_iva_sobretasa, a5_percepcion, a6_iva_retencion, a8_conceptos_no_gravados, a9_compras_no_inscriptas]);
+                a1_neto.value = parseFloat((a11_total.value - a9_compras_no_inscriptas.value - a8_conceptos_no_gravados.value - a7_impuestos_internos.value - a6_iva_retencion.value - a5_percepcion.value) / (1 + a2_iva.value) * 100).toFixed(2);
+                a3_iva_liquidado.value = parseFloat(a1_neto.value * (a2_iva.value / 100)).toFixed(2);
+                break;
+            case '4':
+                clearFields([a1_neto, a3_iva_liquidado, a4_iva_sobretasa, a5_percepcion, a6_iva_retencion, a7_impuestos_internos, a9_compras_no_inscriptas]);
+                a8_conceptos_no_gravados.value = parseFloat(a11_total.value - a9_compras_no_inscriptas.value - a7_impuestos_internos.value - a6_iva_retencion.value - a5_percepcion.value).toFixed(2);
+                break;
+            case '5':
+                clearFields([a1_neto, a3_iva_liquidado, a4_iva_sobretasa, a5_percepcion, a6_iva_retencion, a7_impuestos_internos, a8_conceptos_no_gravados, a9_compras_no_inscriptas]);
+                a3_iva_liquidado.value = parseFloat(a11_total.value).toFixed(2);
+                break;
+            default:
+                console.log(`No op available`);
+        }
+    }
+
+    $("#tipo_calculo, #total").focusout(function() {
+        calcular();
     });
-  }
 
-  $("#tipo_calculo").focusout(function() {
+    //   $("#tipo_calculo").focusout(function() {
 
-    switch (document.getElementById("tipo_calculo").value) {
-      case '1':
-        clearFields([a1_neto, a3_iva_liquidado, a4_iva_sobretasa, a5_percepcion, a6_iva_retencion, a8_conceptos_no_gravados, a9_compras_no_inscriptas]);
-        a1_neto.value = parseFloat((a11_total.value - a9_compras_no_inscriptas.value - a8_conceptos_no_gravados.value - a7_impuestos_internos.value - a6_iva_retencion.value - a5_percepcion.value) / (1 + a2_iva.value) * 100).toFixed(2);
-        a3_iva_liquidado.value = parseFloat(a1_neto.value * (a2_iva.value / 100)).toFixed(2);
-        break;
-      case '4':
-        clearFields([a1_neto, a3_iva_liquidado, a4_iva_sobretasa, a5_percepcion, a6_iva_retencion, a7_impuestos_internos, a9_compras_no_inscriptas]);
-        a8_conceptos_no_gravados.value = parseFloat(a11_total.value - a9_compras_no_inscriptas.value - a7_impuestos_internos.value - a6_iva_retencion.value - a5_percepcion.value).toFixed(2);
-        break;
-      case '5':
-        clearFields([a1_neto, a3_iva_liquidado, a4_iva_sobretasa, a5_percepcion, a6_iva_retencion, a7_impuestos_internos, a8_conceptos_no_gravados, a9_compras_no_inscriptas]);
-        a3_iva_liquidado.value = parseFloat(a11_total.value).toFixed(2);
-        break;
-      default:
-        console.log(`No op available`);
-    }
-
-
-  });
+    //     switch (document.getElementById("tipo_calculo").value) {
+    //       case '1':
+    //         clearFields([a1_neto, a3_iva_liquidado, a4_iva_sobretasa, a5_percepcion, a6_iva_retencion, a8_conceptos_no_gravados, a9_compras_no_inscriptas]);
+    //         a1_neto.value = parseFloat((a11_total.value - a9_compras_no_inscriptas.value - a8_conceptos_no_gravados.value - a7_impuestos_internos.value - a6_iva_retencion.value - a5_percepcion.value) / (1 + a2_iva.value) * 100).toFixed(2);
+    //         a3_iva_liquidado.value = parseFloat(a1_neto.value * (a2_iva.value / 100)).toFixed(2);
+    //         break;
+    //       case '4':
+    //         clearFields([a1_neto, a3_iva_liquidado, a4_iva_sobretasa, a5_percepcion, a6_iva_retencion, a7_impuestos_internos, a9_compras_no_inscriptas]);
+    //         a8_conceptos_no_gravados.value = parseFloat(a11_total.value - a9_compras_no_inscriptas.value - a7_impuestos_internos.value - a6_iva_retencion.value - a5_percepcion.value).toFixed(2);
+    //         break;
+    //       case '5':
+    //         clearFields([a1_neto, a3_iva_liquidado, a4_iva_sobretasa, a5_percepcion, a6_iva_retencion, a7_impuestos_internos, a8_conceptos_no_gravados, a9_compras_no_inscriptas]);
+    //         a3_iva_liquidado.value = parseFloat(a11_total.value).toFixed(2);
+    //         break;
+    //       default:
+    //         console.log(`No op available`);
+    //     }
 
 
+    //   });
 
-  // fetch all data and filters
-  const getData = async () => {
-    const response = await fetch("/api/v1/clientes/listado");
-    const data = await response.json();
-    dataGlobal = data;
-    data.map(element => {
-      globalClientClienteCode.push(element.id.toString())
-      globalClientClienteName.push(element.name)
-      globalClientClienteCuit.push(element.cuit)
+
+
+    // fetch all data and filters
+    const getData = async () => {
+        const response = await fetch("/api/v1/clientes/listado");
+        const data = await response.json();
+        dataGlobal = data;
+        data.map(element => {
+            globalClientClienteCode.push(element.id.toString())
+            globalClientClienteName.push(element.name)
+            globalClientClienteCuit.push(element.cuit)
+        });
+        console.log('dataGlobal :>> ', dataGlobal);
+        return dataGlobal;
+    };
+    (async () => {
+        await getData();
+    })();
+    //=============================================================
+
+    //Each field autocomplete
+    $("#client_id").autocomplete({
+        source: globalClientClienteCode,
+        select: function(event, ui) { //ui.item -> label and value
+            dataGlobal.map(element => { //element = cada obj cliente
+                if (element.id === parseInt(ui.item.value)) {
+                    document.getElementById("client_id").value = parseInt(element.id)
+                    document.getElementById("name").value = element.name
+                    document.getElementById("cuit").value = element.cuit
+                    document.getElementById("condition").value = element.condition
+                }
+            });
+        }
     });
-    console.log('dataGlobal :>> ', dataGlobal);
-    return dataGlobal;
-  };
-  (async () => {
-    await getData();
-  })();
-  //=============================================================
-
-  //Each field autocomplete
-  $("#client_id").autocomplete({
-    source: globalClientClienteCode,
-    select: function(event, ui) { //ui.item -> label and value
-      dataGlobal.map(element => { //element = cada obj cliente
-        if (element.id === parseInt(ui.item.value)) {
-          document.getElementById("client_id").value = parseInt(element.id)
-          document.getElementById("name").value = element.name
-          document.getElementById("cuit").value = element.cuit
-          document.getElementById("condition").value = element.condition
+    //=======================
+    $("#name").autocomplete({
+        source: globalClientClienteName,
+        select: function(event, ui) { //ui.item -> label and value
+            dataGlobal.map(element => { //element = cada obj cliente            
+                if (element.name === ui.item.value) {
+                    document.getElementById("client_id").value = element.id
+                    document.getElementById("name").value = element.name
+                    document.getElementById("cuit").value = element.cuit
+                    document.getElementById("condition").value = element.condition
+                }
+            });
         }
-      });
-    }
-  });
-  //=======================
-  $("#name").autocomplete({
-    source: globalClientClienteName,
-    select: function(event, ui) { //ui.item -> label and value
-      dataGlobal.map(element => { //element = cada obj cliente            
-        if (element.name === ui.item.value) {
-          document.getElementById("client_id").value = element.id
-          document.getElementById("name").value = element.name
-          document.getElementById("cuit").value = element.cuit
-          document.getElementById("condition").value = element.condition
+    });
+    //=======================
+    $("#cuit").autocomplete({
+        source: globalClientClienteCuit,
+        select: function(event, ui) { //ui.item -> label and value
+            dataGlobal.map(element => { //element = cada obj cliente
+                if (element.cuit === ui.item.value) {
+                    document.getElementById("client_id").value = element.id
+                    document.getElementById("name").value = element.name
+                    document.getElementById("cuit").value = element.cuit
+                    document.getElementById("condition").value = element.condition
+                }
+            });
         }
-      });
-    }
-  });
-  //=======================
-  $("#cuit").autocomplete({
-    source: globalClientClienteCuit,
-    select: function(event, ui) { //ui.item -> label and value
-      dataGlobal.map(element => { //element = cada obj cliente
-        if (element.cuit === ui.item.value) {
-          document.getElementById("client_id").value = element.id
-          document.getElementById("name").value = element.name
-          document.getElementById("cuit").value = element.cuit
-          document.getElementById("condition").value = element.condition
-        }
-      });
-    }
-  });
-  //=======================
+    });
+    //=======================
 </script>
 
 
