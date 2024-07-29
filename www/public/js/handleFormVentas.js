e7566dcc3942d659ff6fa7693b4464a768515888
jona872 <jona_872@hotmail.com>
2024-07-28 22:50:08 -0300

    ventas clear

--- www/public/js/handleFormVentas.js
+++ www/public/js/handleFormVentas.js
@@ -15,6 +15,7 @@ let a7_conceptos_no_gravados = document.getElementById('conceptos_no_gravados');
 let a8_ingresos_exentos = document.getElementById('ingresos_exentos');
 let a9_ganancias_retencion = document.getElementById('ganancias_retencion');
 let a11_total = document.getElementById('total');
+let btn_limpiar = document.getElementById('limpiar');
 
 
 // let editableButtons = [ a1_neto, a3_iva_liquidado, a4_iva_sobretasa, a5_percepcion, a6_iva_retencion, a7_impuestos_internos, a8_ingresos_exentos, a9_ganancias_retencion ];
@@ -25,26 +26,45 @@ function clearFields(vButtons) {
   });
 }
 
+function limpiarNumeros() {
+  clearFields([a1_neto, a3_iva_liquidado, a4_iva_sobretasa, a5_percepcion, a6_iva_retencion, a7_conceptos_no_gravados, a8_ingresos_exentos, a9_ganancias_retencion]);
+}
+btn_limpiar.addEventListener('click', limpiarNumeros);
+
 function calcular() {
+  let total = parseFloat(a11_total.value) || 0;
+  let iva = parseFloat(a2_iva.value) || 0;
+  let percepcion = parseFloat(a5_percepcion.value) || 0;
+  let ivaRetencion = parseFloat(a6_iva_retencion.value) || 0;
+  let conceptosNoGravados = parseFloat(a7_conceptos_no_gravados.value) || 0;
+  let ingresosExentos = parseFloat(a8_ingresos_exentos.value) || 0;
+  let gananciasRetencion = parseFloat(a9_ganancias_retencion.value) || 0;
+
   switch (document.getElementById("tipo_op").value) {
     case '1':
-      clearFields([a1_neto, a3_iva_liquidado, a4_iva_sobretasa, a5_percepcion, a6_iva_retencion, a8_ingresos_exentos, a9_ganancias_retencion]);
-      a1_neto.value = parseFloat((a11_total.value - a9_ganancias_retencion.value - a8_ingresos_exentos.value - a7_conceptos_no_gravados.value - a6_iva_retencion.value - a5_percepcion.value) / (1 + a2_iva.value) * 100).toFixed(2);
-      a3_iva_liquidado.value = parseFloat(a1_neto.value * (a2_iva.value / 100)).toFixed(2);
+      clearFields([a1_neto, a3_iva_liquidado]);
+      // a1_neto.value = parseFloat((total - a9_ganancias_retencion.value - a8_ingresos_exentos.value - a7_conceptos_no_gravados.value - ivaRetencion - percepcion) / (1 + iva) * 100).toFixed(2);
+      // a3_iva_liquidado.value = parseFloat(a1_neto.value * (iva / 100)).toFixed(2);
+      a1_neto.value = parseFloat((total - conceptosNoGravados - ingresosExentos - gananciasRetencion - percepcion - ivaRetencion) / (1 + iva / 100)).toFixed(2);
+      a3_iva_liquidado.value = parseFloat(a1_neto.value * (iva / 100)).toFixed(2);
+      break;
       break;
     case '4':
       clearFields([a1_neto, a3_iva_liquidado, a4_iva_sobretasa, a5_percepcion, a6_iva_retencion, a7_conceptos_no_gravados, a9_ganancias_retencion]);
-      a8_ingresos_exentos.value = parseFloat(a11_total.value - a9_ganancias_retencion.value - a7_conceptos_no_gravados.value - a6_iva_retencion.value - a5_percepcion.value).toFixed(2);
+      a8_ingresos_exentos.value = parseFloat(total - a9_ganancias_retencion.value - a7_conceptos_no_gravados.value - ivaRetencion - percepcion).toFixed(2);
       break;
     case '5':
       clearFields([a1_neto, a3_iva_liquidado, a4_iva_sobretasa, a5_percepcion, a6_iva_retencion, a7_conceptos_no_gravados, a8_ingresos_exentos, a9_ganancias_retencion]);
-      a3_iva_liquidado.value = parseFloat(a11_total.value).toFixed(2);
+      a3_iva_liquidado.value = parseFloat(total).toFixed(2);
       break;
     default:
       console.log(`No op available`);
   }
 }
 $("#tipo_op, #total").focusout(function () {
+  calcular();
+});
+$("#conceptos_no_gravados, #ingresos_exentos, #retencion_ganancias, #percepcion, #iva_retencion, #tipo_calculo, #total").focusout(function () {
   calcular();
 });
 
